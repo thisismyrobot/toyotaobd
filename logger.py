43ff7158e29102b1c3eb12df666d58c249f1decb
Robert Wallhead <rwallhead@gmail.com>
2012-11-25 12:19:31 +1100

    Fixed up code after actually running it through an interpreter :)

--- logger.py
+++ logger.py
@@ -2,30 +2,33 @@ import serial
 import time
 
 
-COM = "COM9"
+COM = "/dev/ttyUSB0"
 
 maps = {"0C": ("RPM", lambda a, b: (a * 256 + b) / 4)}
 
+
 # main stuff
 ser = serial.Serial(COM, timeout=1)
-for hex, (units, func) in maps:
+for hex, (units, func) in maps.items():
 
     # prepare the outgoing request
     req = "01{0}".format(hex)
 
-    print " > {0}" + req
+    print " > {0}".format(req)
 
     # send the request
-    ser.write("{0}\n".format(req));
-    ser.readline() # the echo of sent data
+    ser.write("{0}\n".format(req))
 
-    # the data, sans newline and header data
-    res = ser.readline().trim().split(" ")[2:]
+    # discard the echo of sent data
+    ser.readline()
 
-    print " < {0}" + res
+    # the data, sans newline and header data, parsed into tokens
+    res = ser.readline().strip().split(" ")[2:]
+
+    print " < {0}".format(res)
 
     # parse the result into token, de-hexifier
-    tokens = [ord(t) for t in res.split(" ")]
+    tokens = [ord(t) for t in res]
     val = func(*tokens)
 
     print " = {0} {1}".format(val, units)
