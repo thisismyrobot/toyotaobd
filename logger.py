b905b2de1b37338b953e7fb5ff09be40fbef5bef
Robert Wallhead <rwallhead@gmail.com>
2012-11-25 12:52:28 +1100

    Works! shows the RPM correctly.
    Required some fixes for the ord code and serial reads

--- logger.py
+++ logger.py
@@ -6,30 +6,35 @@ COM = "/dev/ttyUSB0"
 
 maps = {"0C": ("RPM", lambda a, b: (a * 256 + b) / 4)}
 
+# set up
+ser = serial.Serial(COM, timeout=1)
+ser.write("ATZ\r")
+time.sleep(4)
+print ser.readline()
+print ser.readline()
+print ser.readline()
+
 
 # main stuff
-ser = serial.Serial(COM, timeout=1)
-for hex, (units, func) in maps.items():
+while True:
+    for hex, (units, func) in maps.items():
 
-    # prepare the outgoing request
-    req = "01{0}".format(hex)
+        # prepare the outgoing request
+        req = "01{0}".format(hex)
 
-    print " > {0}".format(req)
+        print " > {0}".format(req)
 
-    # send the request
-    ser.write("{0}\n".format(req))
+        # send the request
+        ser.write("{0}\r".format(req))
 
-    # discard the echo of sent data
-    ser.readline()
+        # the data, sans newline and header data, parsed into tokens
+        res = ser.readline().strip().split(" ")[2:-1]
 
-    # the data, sans newline and header data, parsed into tokens
-    res = ser.readline().strip().split(" ")[2:]
+        print " < {0}".format(res)
 
-    print " < {0}".format(res)
+        # parse the result into token, de-hexifier
+        tokens = [ord(chr(int(t, 16))) for t in res]
+        val = func(*tokens)
 
-    # parse the result into token, de-hexifier
-    tokens = [ord(t) for t in res]
-    val = func(*tokens)
-
-    print " = {0} {1}".format(val, units)
-    time.sleep(1000)
+        print "{0} {1}".format(val, units)
+        time.sleep(0.1)
